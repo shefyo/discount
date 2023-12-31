b9247c1514f5cbc4423300e5e5250da73804c726
shefyo <110970148+shefyo@users.noreply.github.com>
2023-12-31 21:26:45 +0300

    Убрана переменная discount

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,10 @@
 fun main() {
     val purchaseAmount = 1001
     val isRegularCustomer = true
-    var discount = 0
     var finalAmount = purchaseAmount
 
     if (purchaseAmount > 0 && purchaseAmount <= 1000) {
-        discount = 0
+        finalAmount = finalAmount
     } else if (purchaseAmount > 1000 && purchaseAmount <= 10000) {
         finalAmount -= 100
     } else if (purchaseAmount > 10000) {
